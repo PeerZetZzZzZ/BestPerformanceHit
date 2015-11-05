ad7b4ae4496b7443ee5c53b8b7f40cf83acc42a0
Przemyslaw Thomann <peerzet3@gmail.com>
2015-11-05 19:24:40 +0000

    added hello world example

---
+++ bestperformancehit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+class BestPerformanceHit:
+    def hello(self):
+        print('Welcome to the world of python ;)')
+
+
+
+# This is outside the class
+k = BestPerformanceHit()
+k.hello()
